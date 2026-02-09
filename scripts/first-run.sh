25e49b5e4a750b1fd8f22db65753975b2764406a
Meshak Kukeba <mes.ku@protonmail.com>
2026-02-09 22:09:37 +0000

    Start using postAttach so we csn actually see secrets
    Running the playbook beforehand results in a very stripped down environment

--- scripts/first-run.sh
+++ scripts/first-run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 git clone https://github.com/meskuk/ansible-csc
 cd ansible-csc
+# Requires nopasswd sudo since it's for postAttachCommand
 ansible-playbook cloudflared/cloudflared.yaml whoami/whoami.yaml
+sudo supervisorctl update all
